c095ed236be70de70d270d2cb65d94818f33f5af
eseferr <erginsefer@gmail.com>
2024-12-24 15:11:26 +0300

    Add REPL structure

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
 
 func main(){
-fmt.Println(cleanInput("Hello, World!"))
+startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,24 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+
+func startRepl(){
+	scanner:= bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("\nPokedex > ")
+		scanner.Scan()
+		userInput := cleanInput(scanner.Text())
+		fmt.Printf("Your command was: %s", userInput[0])
+	}
+}
+
+
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
